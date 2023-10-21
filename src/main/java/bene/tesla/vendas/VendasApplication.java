9f696166a9d5366c50471cbc328122f04fc2eb52
Bene-Tesla <benevaniosantos930@gmail.com>
2023-10-21 18:23:40 -0300

    :sparkles: usando as variaveis de ambiente em nosso sistema

--- src/main/java/bene/tesla/vendas/VendasApplication.java
+++ src/main/java/bene/tesla/vendas/VendasApplication.java
@@ -1,26 +1,22 @@
 package bene.tesla.vendas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"bene.tesla"})	
 @RestController
 public class VendasApplication {
-
-@Autowired
-@Qualifier("applicationName")
-private String applicationName;
+	@Value("${application.name}")
+	private String applicationName;
 
 	@GetMapping("/")
 	public String hello() {
-		return applicationName;
+		return "Hello World! " + applicationName;
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
